0fd860b9a81f685171c0ce09fa2d2c004d187bc8
Ryan Ringler <rringler@gmail.com>
2017-01-28 19:58:51 -0800

    Seed database before running specs
    Seed database before running specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,6 +66,8 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+
+    Rails.application.load_seed
   end
 
   config.before(:each) { DatabaseCleaner.start }
