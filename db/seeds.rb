20468f478928b8d08f07ba22427aef87acb176ca
Ryan Ringler <rringler@gmail.com>
2017-03-18 09:19:36 -0700

    Move State two-letter code definitions into a constant in the class

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-STATE_CODES = [
-  'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MH', 'MA', 'MI', 'FM', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'VI', 'WA', 'WV', 'WI', 'WY'
-].freeze
-
-STATE_CODES.each { |code| State.find_or_create_by!(code: code) }
+State::CODES.each { |code| State.find_or_create_by!(code: code) }
